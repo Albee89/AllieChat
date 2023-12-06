a449e898ace8fdeb38aa6ce045a16e4618685a45
Alex B <137462408+Albee89@users.noreply.github.com>
2023-12-06 15:00:40 +1000

    Update allieChat.js

--- allieChat.js
+++ allieChat.js
@@ -3,27 +3,33 @@
 // Establish a connection to the SocketIO server
 var socket = io.connect('http://' + document.domain + ':' + location.port);
 
-// Function to send a message to the server
+// Function to send a message to the server:
 function sendMessage() {
-    // Get user input
-    var userInput = document.getElementById("userInput").value;
+    // Getting user input:
+    var userInput = document.getElementById("userInput").value.trim(); // Trim to remove leading and trailing spaces
 
-    // Emit a 'message' event with the user input
-    socket.emit('message', { user_input: userInput });
+    // Checking if the user input is not empty:
+    if (userInput !== "") {
+        // Emitting a 'message' event with the user input:
+        socket.emit('message', { user_input: userInput });
 
-    // Log the user's input to the console
-    console.log('User Input:', userInput);
+        // Logging the user's input to the console:
+        console.log('User Input:', userInput);
+
+        // Clearing the input field:
+        document.getElementById("userInput").value = "";
+    }
 }
 
-// Event listener for incoming messages
+// Event listener for incoming messages:
 socket.on('message', function(data) {
-    // Check if the bot response is empty or null
+    // Checking if the bot response is empty or null:
     if (!data.bot_response) {
         console.log('Chat bot did not respond.'); // Log to the console
-        return; // Exit the function
+        return; // Exiting the function
     }
 
-    // Log the user's input and bot response to the console
+    // Logging the user's input and bot response to the console:
     console.log('User Input:', data.user_input);
     console.log('Bot Response:', data.bot_response);
 
